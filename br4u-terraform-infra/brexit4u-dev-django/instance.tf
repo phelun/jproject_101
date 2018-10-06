cedf61f6ead774f5fa7b7119b1fb9f570ff4dad1
ansible <ansible@codename12>
2018-10-06 11:58:39 +0100

    Fixed terraform query for ami

--- br4u-terraform-infra/brexit4u-dev-django/instance.tf
+++ br4u-terraform-infra/brexit4u-dev-django/instance.tf
@@ -1,4 +1,4 @@
-resource "aws_instance" "example" {
+resource "aws_instance" "dev_inst" {
   ami           = "${data.aws_ami.amzn_linux.image_id}"
   instance_type = "t2.micro"
 }
--- br4u-terraform-infra/brexit4u-dev-django/main.tf
+++ br4u-terraform-infra/brexit4u-dev-django/main.tf
@@ -12,23 +12,14 @@ data "aws_ami" "amzn_linux" {
   most_recent = true
 
   filter {
-    name = "owner-alias"
-    values = ["658951324167"]
+    name = "status"
+    values = ["available"]
   }
 
   filter {
-    name = "root-device-type"
-    values = ["ebs"]
+    name = "tag:environ"
+    values = ["dev"]
   }
 }
 
-# Create an EC2 instance
-#resource "aws_instance" "example" {
-#  ami = "${data.aws_ami.amzn_linux.image_id}"
-#  instance_type = "t2.micro"
-#  key_name = "${var.key_pair_name}"
-#
-#  tags {
-#    Name = "${var.ec2_name}"
-#  }
-#}
+
