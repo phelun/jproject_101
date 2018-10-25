12d29395dd88cc2b989d13c710dc1808bfdc9202
ansible <ansible@codename12>
2018-10-25 21:13:27 +0100

    post delete testing

--- 20_restore_file_from_git.py
+++ 20_restore_file_from_git.py
@@ -2,18 +2,10 @@
 
 from subprocess import check_output, call
 
-
 file_name = str(input('Enter the file name: '))
 
 commit = check_output(["git", "rev-list", "-n", "1", "HEAD", "--", file_name])
+clean_commit = commit.rstrip()
+print(clean_commit)
 
-print(str(commit))
-
-#call(["git", "checkout", str(commit).rstrip()+"~1", file_name])
-call(["git", "checkout", str(commit), file_name])
-
-
-"""
-After entering a filename, this script searches your Git history for that file.
-If the file exists, then it will restore it.
-"""
+call(["git", "checkout", clean_commit, file_name])
